6b876631ecefaf63b5a5d5240e4e34e2a57e684e
yiminghu <yiminghu@hustunique.com>
2016-10-24 19:40:39 +0800

    refactor(project): init

---
+++ include/io.h
@@ -0,0 +1,40 @@
+//
+// Created by 胡一鸣 on 16/10/24.
+//
+
+#ifndef C_PARSER_IO_H
+#define C_PARSER_IO_H
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+constexpr int bufferSize = 10;
+
+using namespace std;
+
+namespace cParser {
+namespace io {
+char result[1000];
+
+inline string read(const string &relativePath) {
+  ifstream f(relativePath);
+  f.read(result, 1000);
+  f.close();
+  return result;
+}
+
+inline void write(const string &relativePath, const string &content) {
+  ofstream f(relativePath);
+  if (f.is_open()) {
+    f << content;
+    f.close();
+  } else {
+    cerr << "cannot open file" << endl;
+  }
+  return;
+}
+}
+};
+
+#endif // C_PARSER_IO_H
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "io.h"
+#include <iostream>
+
+using namespace std;
+using namespace cParser;
+
+int main(int argc, char const *argv[]) { return 0; }
+io::write("test_write", io::read("test"));
+return 0;
+}
