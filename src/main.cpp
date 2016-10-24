a923d0ce1c39ee196679579773ec1fe4f3d89b14
yiminghu <yiminghu@hustunique.com>
2016-10-24 19:53:57 +0800

    build: fix project settings

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 using namespace cParser;
 
-int main(int argc, char const *argv[]) { return 0; }
-io::write("test_write", io::read("test"));
-return 0;
+int main(int argc, char const *argv[]) {
+  io::write("test_write", io::read("test"));
+  return 0;
 }
